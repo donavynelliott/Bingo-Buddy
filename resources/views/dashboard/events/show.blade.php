74b679d9eee2d61fe81889f3c8835e6c625f90e0
Don Elliott <donavyn.elliott@gmail.com>
2023-06-07 06:54:48 -0700

    Show event teams on event page if teams are setup

--- resources/views/dashboard/events/show.blade.php
+++ resources/views/dashboard/events/show.blade.php
@@ -62,21 +62,43 @@ use App\Enums\EventStatus;
                 </div>
 
                 <div class="p-6 text-gray-900">
-                    <h2 class="text-2xl text-gray-900 font-bold mb-4">Event Members</h2>
-                    @php
-                    $users = $event->users;
-                    $userCount = $users->count();
-                    @endphp
-                    <!-- Show 10 users -->
-                    <div class="grid grid-cols-5 gap-4">
-                        <ul class="list-disc">
-                            @foreach ($users->take(10) as $user)
-                            <li class="text-gray-700 text-sm mb-4">{{ $user->name }}</li>
+                    @if ($event->rules->teams && $event->teams->count() > 0)
+                        <h2 class="text-2xl text-gray-900 font-bold mb-4">Event Teams</h2>
+                        <div class="grid grid-cols-4 gap-4">
+                            @foreach ($event->teams as $team)
+                                <div>
+                                    <h2 class="text-1xl text-gray-900 font-bold mb-4">{{ $team->name }}</h2>
+                                    @php
+                                    $users = $team->users;
+                                    $userCount = $users->count();
+                                    @endphp
+
+                                    <!-- Show 10 users -->
+                                    <ul class="list-disc">
+                                        @foreach ($users->take(10) as $user)
+                                        <li class="text-gray-700 text-sm mb-4">{{ $user->name }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
                             @endforeach
-                        </ul>
-                    </div>
-                    @if ($userCount > 10)
-                    <a href="{{ route('events.members', ['event' => $event]) }}" class="text-gray-700 text-sm mb-4">And {{ $userCount - 10 }} more...</a>
+                        </div>
+                    @else
+                        <h2 class="text-2xl text-gray-900 font-bold mb-4">Event Members</h2>
+                        @php
+                        $users = $event->users;
+                        $userCount = $users->count();
+                        @endphp
+                        <!-- Show 10 users -->
+                        <div class="grid grid-cols-5 gap-4">
+                            <ul class="list-disc">
+                                @foreach ($users->take(10) as $user)
+                                <li class="text-gray-700 text-sm mb-4">{{ $user->name }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @if ($userCount > 10)
+                        <a href="{{ route('events.members', ['event' => $event]) }}" class="text-gray-700 text-sm mb-4">And {{ $userCount - 10 }} more...</a>
+                        @endif
                     @endif
 
                     @if ($event->user_id == auth()->id() && $event->status->is(EventStatus::NotStarted) && $event->rules->teams)
